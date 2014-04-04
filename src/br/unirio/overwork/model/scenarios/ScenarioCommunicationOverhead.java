f4354c772cdc9a4de245607f8ec9ffaa74ac9236
Luiz Antonio <luizbr84@gmail.com>
2014-04-03 23:50:42 -0300

    Criação do cenário de communication overhead.

---
+++ src/br/unirio/overwork/model/scenarios/ScenarioCommunicationOverhead.java
@@ -0,0 +1,40 @@
+package br.unirio.overwork.model.scenarios;
+
+import java.util.ArrayList;
+
+import br.unirio.overwork.model.Activity;
+import br.unirio.overwork.model.Developer;
+import br.unirio.overwork.simulation.Scenario;
+import br.unirio.overwork.simulation.Tables;
+
+public class ScenarioCommunicationOverhead  extends Scenario<Activity>{
+
+	/**
+	 * Table depicting the decrease over productivity due communication overhead
+	 */
+	private static double[] COMMUNICATION_OVERHEAD_FACTOR = {0, 0.015, 0.06, 0.135, 0.24, 0.375, 0.54};
+	
+	/**
+	 * Initializes the scenario
+	 */
+	public ScenarioCommunicationOverhead()
+	{
+		
+	}
+	
+	/**
+	 * Prepares the scenario for execution
+	 */
+	@Override
+	public void init(Activity activity)
+	{
+		ArrayList<Developer> countDeveloper = (ArrayList<Developer>) activity.getProject().getDevelopers();
+		float val = 1/7;
+		int countDeveloperModifier = Math.round(countDeveloper.size()/5);
+		double communicationOverheadFactor = COMMUNICATION_OVERHEAD_FACTOR[countDeveloperModifier];
+				
+		activity.setProductivity(activity.getProductivity() * (1-communicationOverheadFactor));
+		activity.setCommunicationOverheadRate(communicationOverheadFactor); 
+	
+	}
+}

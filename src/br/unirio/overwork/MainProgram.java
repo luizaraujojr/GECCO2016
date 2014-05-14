7c9f8cb22dd89a9fdfc5201fdede1068a62c6633
Luiz Antonio <luizbr84@gmail.com>
2014-05-14 11:58:04 -0300

    Update

--- src/br/unirio/overwork/MainProgram.java
+++ src/br/unirio/overwork/MainProgram.java
@@ -1,59 +1,38 @@
 package br.unirio.overwork;
 
-import java.io.OutputStreamWriter;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
 import java.util.Vector;
 
-import unirio.experiments.monoobjective.execution.FileMonoExperimentListener;
-import unirio.experiments.monoobjective.execution.StreamMonoExperimentListener;
-import br.unirio.optimization.ProjectSolution;
 import br.unirio.optimization.RandomSearchExperiment;
-import br.unirio.overwork.builders.WorkPackageProjectBuilder;
-<<<<<<< HEAD
-import br.unirio.overwork.model.Developer;
-import br.unirio.overwork.model.Project;
-=======
-import br.unirio.overwork.instance.reader.InstanceReader;
 import br.unirio.overwork.model.base.Developer;
 import br.unirio.overwork.model.base.Project;
-import br.unirio.overwork.model.scenarios.ScenarioExhaution;
->>>>>>> 01b098677229606f1983375d5beed6ee3ca623ee
-import br.unirio.overwork.model.scenarios.ScenarioOverworking;
 import br.unirio.reader.InstanceReader;
 import br.unirio.simulation.SimulationEngine;
 
 public class MainProgram
 {
 	protected static final int CYCLES = 10;
-	protected static String[] instanciasAleatorias =
+	protected static String[] instanceFiles =
 		{
-	 		"C:\\workspace\\OptimizationAlgorithms\\trunk\\data\\Instance.txt",
+	 		"data/instances/ACAD/functions-point.xml",
+	 		"data/instances/BOLS/functions-point.xml",
+	 		"data/instances/PARM/functions-point.xml",
+	 		"data/instances/PSOA/functions-point.xml"
 		};
 	
 
 	
 	public static final void main(String[] args) throws Exception
 	{
-<<<<<<< HEAD
-		for (int i = 0; i < instanciasAleatorias.length; i++)
-			if (instanciasAleatorias[i].length() > 0)
-				run(instanciasAleatorias[i]);
-=======
-		InstanceReader reader = new InstanceReader();
-		reader.run("data/instances/ACAD/functions-point.xml");
-		reader.run("data/instances/BOLS/functions-point.xml");
-		reader.run("data/instances/PARM/functions-point.xml");
-		reader.run("data/instances/PSOA/functions-point.xml");
+		for (int i = 0; i < instanceFiles.length; i++)
+			if (instanceFiles[i].length() > 0)
+				run(instanceFiles[i]);
 		
 		Project project = createProject();
 		
 		SimulationEngine simulator = new SimulationEngine();
 
 		for (Developer developer : project.getDevelopers())
-			simulator.addResource(developer.getEffort());
->>>>>>> 01b098677229606f1983375d5beed6ee3ca623ee
-		
+			simulator.addResource(developer.getEffort());		
 		
 		
 		String overtime = "";

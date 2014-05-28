e54d5288ade43fe32014aeadef81239e6e88d02b
Marcio Barros <marcio.barros@gmail.com>
2014-05-28 20:45:09 -0300

    Relatórios e casos de teste do Padilha

--- src/br/unirio/overwork/instance/MainInstanceLoader.java
+++ src/br/unirio/overwork/instance/MainInstanceLoader.java
@@ -3,15 +3,17 @@ package br.unirio.overwork.instance;
 import br.unirio.overwork.instance.calculator.FunctionPointCalculator;
 import br.unirio.overwork.instance.model.FunctionPointSystem;
 import br.unirio.overwork.instance.reader.InstanceReader;
+import br.unirio.overwork.instance.report.Report;
+import br.unirio.overwork.instance.report.TestCasePublisher;
 
 public class MainInstanceLoader
 {
 	private static String[] instanceFiles =
 	{
-	 	"data/instances/ACAD/functions-point.xml",
+//	 	"data/instances/ACAD/functions-point.xml",
 //	 	"data/instances/BOLS/functions-point.xml",
 //	 	"data/instances/PARM/functions-point.xml",
-//	 	"data/instances/PSOA/functions-point.xml"
+	 	"data/instances/PSOA/functions-point.xml"
 	};
 	
 	public static final void main(String[] args) throws Exception
@@ -20,8 +22,12 @@ public class MainInstanceLoader
 
 		for (int i = 0; i < instanceFiles.length; i++)
 		{
-			FunctionPointSystem fps = reader.run(instanceFiles[i]);
-			new FunctionPointCalculator().calculate(fps);
+			String instanceFilename = instanceFiles[i];
+			FunctionPointSystem fps = reader.run(instanceFilename);
+			Report report = new FunctionPointCalculator().calculate(fps);
+
+			String instanceName = instanceFilename.substring(15, instanceFilename.lastIndexOf('/'));
+			new TestCasePublisher().run(report, instanceFilename, "TestReader" + instanceName);
 		}
 	}
 }

d317cc91c93743a72d2850f2becea78b430b7ad7
Marcio Barros <marcio.barros@gmail.com>
2014-07-24 21:41:34 -0300

    Análise de OMS

--- src/br/unirio/optimization/analysis/MainProgram.java
+++ src/br/unirio/optimization/analysis/MainProgram.java
@@ -13,7 +13,8 @@ public class MainProgram
 		Locale.setDefault(new Locale("en", "US"));
 		//analyzeStopCriteria();
 		//analyzePopulationSize();
-		analyzeRandomSearch();
+		//analyzeRandomSearch();
+		analyzeOMS();
 	}
 
 	protected static void analyzeStopCriteria() throws ExperimentFileReaderException, Exception
@@ -43,6 +44,17 @@ public class MainProgram
 		MultiExperimentAnalyzer analyzer = new MultiExperimentAnalyzer();
 		analyzer.addExperimentResult(reader.execute("nsga50k2x", "data/result/NSGA/50K/2X/nsga50k2x.txt", 6, 50, 3));
 		analyzer.addExperimentResult(reader.execute("rs50k", "data/result/RS/50K/rs50k.txt", 6, 50, 3));
+		analyzer.analyzeCycleFrontiers();
+	}
+
+	protected static void analyzeOMS() throws ExperimentFileReaderException, Exception
+	{
+		ExperimentFileReader reader = new ExperimentFileReader();
+		MultiExperimentAnalyzer analyzer = new MultiExperimentAnalyzer();
+		analyzer.addExperimentResult(reader.execute("GA", "data/result/NSGA/50K/2X/nsga50k2x.txt", 6, 50, 3));
+		analyzer.addExperimentResult(reader.execute("MAR", "data/result/OMS/Margarine/Margarine.txt", 6, 1, 3));
+		analyzer.addExperimentResult(reader.execute("SH", "data/result/OMS/SecondHalf/SecondHalf.txt", 6, 1, 3));
+		analyzer.addExperimentResult(reader.execute("CPM", "data/result/OMS/CPM/CPM.txt", 6, 1, 3));
 		analyzer.analyzeCycleFrontiers();
 	}
 }
